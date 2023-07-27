23f2ae8b0c98528a9c299cd131ae13fd6072801e
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2023-07-27 00:09:43 +0000

    updated backend main endpoint

--- src/shared/helpers/axios/axiosInstance.jsx
+++ src/shared/helpers/axios/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://classroom-assistant-backend.onrender.com",
 });
 
 export default axiosInstance;
