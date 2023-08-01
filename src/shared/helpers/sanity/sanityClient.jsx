41c27b1e33f16b70861fb902e4025c1a3a25a097
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2023-08-01 05:00:13 +0000

    modified env file

--- src/shared/helpers/sanity/sanityClient.jsx
+++ src/shared/helpers/sanity/sanityClient.jsx
@@ -1,10 +1,9 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "byu7n5l4",
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2023-08-01",
   useCdn: true,
-  token:
-    "skWPuyUxSHRKzhedkCMBd6hiQXQtsH9QEkKaZvl2jqpYs2cMHrT7dYVGyokKeuDvZeluI5T61Kh8oMk0BsWbc0E432svXAGt0WAovVEMrIDQEaFrorm6Z7PhBfQyap33n21sA6Gkku6IU08ilwNHLSofRJWs4v4ugzKeg6CrdZ29KrWolv09",
+  token: import.meta.env.VITE_SANITY_TOKEN,
 });
